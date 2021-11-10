62adf7f37e15de5ffc627255581848b028a2ff77
Peyton Anderson <peytonianderson@gmail.com>
2021-11-10 17:34:35 -0500

    Part D

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -6,10 +6,6 @@ const loginPage = (req, res) => {
   res.render('login', { csrfToken: req.csrfToken() });
 };
 
-const signupPage = (req, res) => {
-  res.render('signup', { csrfToken: req.csrfToken() });
-};
-
 const logout = (req, res) => {
   req.session.destroy();
   res.redirect('/');
@@ -77,8 +73,16 @@ const signup = (req, res) => {
   });
 };
 
+const getToken = (req, res) => {
+    const csrfJSON = {
+        csrfToken: req.csrfToken(),
+    };
+
+    res.json(csrfJSON);
+};
+
 module.exports.loginPage = loginPage;
 module.exports.login = login;
 module.exports.logout = logout;
-module.exports.signupPage = signupPage;
 module.exports.signup = signup;
+module.exports.getToken = getToken;
